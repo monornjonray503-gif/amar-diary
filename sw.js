c0bd5d29cff866aacafe74b9f049d2938db07b59
monornjonray503-gif <monornjonray503@gmail.com>
2026-09-22 10:42:31 +0600

    sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "amar-diary-v1";
+
+const APP_SHELL = [
+  "/",
+  "/index.html",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(response =>
+        response || caches.match("/index.html")
+      )
+    )
+  );
+});
